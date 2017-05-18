1da07810b4650a088132ce3b965158c0a1d0f5a1
TES Operator <xf08bm@xf08bm-ws1.cs.nsls2.local>
2017-05-18 11:46:11 -0400

    Add escan plan

---
+++ startup/90-setup.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import bluesky.plans as bp
+from bluesky.callbacks import LiveTable, LivePlot
+
+
+
+def escan(start, stop, num, md=None):
+    """
+    Scan the mono_energy while reading the scaler.
+
+    Parameters
+    ----------
+    start : number
+    stop : number
+    num : integer
+        number of data points (i.e. number of strides + 1)
+    md : dictionary, optional
+    """
+    dets = [sclr]
+    motor = mono_energy
+    cols = ['I0', 'fbratio', 'It', 'If_tot']
+    x = 'mono_energy'
+    fig, axes = plt.subplots(2, sharex=True)
+
+    plan = bp.scan(dets, motor, start, stop, num, md=md)
+    plan2 = bp.subs_wrapper(plan, [LiveTable(cols),
+                                   LivePlot('If_tot', x, ax=axes[0]),
+                                   LivePlot('I0', x, ax=axes[1])])
+    yield from plan2
