acc59802bbb2bb21f3237e7666a1c02bb19f98bc
Thomas A Caswell <tcaswell@bnl.gov>
2018-01-09 15:44:54 -0500

    ENH: overhaul scaler implemetation
     - use channel-first class as base
     - add class for dealing with the MCA (not the Mark Rivers MCA)
       - can not find docs, but extracted PVs from mdm + IDL code
     - combining counting and MCA controls in single modal Device

--- startup/25-sclr.py
+++ startup/25-sclr.py
@@ -1,35 +1,110 @@
-import time
-from ophyd import EpicsScaler, EpicsSignal, EpicsSignalRO
+from ophyd.scaler import ScalerCH
+from ophyd.device import (Component as C, DynamicDeviceComponent as DDC,
+                          FormattedComponent as FC)
+from ophyd.status import StatusBase
 
-class DirtyLyingEpicsScaler(EpicsScaler):
+
+class ScalerMCA(Device):
+    _default_read_attrs = ('channels', 'current_channel')
+    _default_configuration_attrs = ('nuse', 'prescale')
+    
+    channels = DDC({f'mca{k:02d}': (EpicsSignal, f"mca{k}", {}) for k in range(1, 21)})
+    startall = C(EpicsSignal, 'StartAll', string=True)
+    stopall = C(EpicsSignal, 'StopAll', string=True)
+    eraseall = C(EpicsSignal, 'EraseAll', string=True)
+    erasestart = C(EpicsSignal, 'EraseStart', string=True)
+
+    current_channel = C(EpicsSignal, 'CurrentChannel')
+    nuse = C(EpicsSignal, 'NuseAll')
+    prescale = C(EpicsSignal, 'Prescale')
+
+    # high is acquiring
+    status = C(EpicsSignal, 'Acquiring', string=True)
+
+    def stage(self):
+        super().stage()
+        self.eraseall.put('Erase')
+
+    def stop(self):
+        self.stopall.put('Stop')
+
     def trigger(self):
-        ret = super().trigger()
-        # The .CNT signal seems to fire before the device is actually up to date
-        # and thus we read a value that is slightly too low! This fixes it.
-        time.sleep(0.1)
-        return ret
+        self.erasestart.put('Erase')
 
-sclr = DirtyLyingEpicsScaler('XF:08BM-ES:1{Sclr:1}scaler1', name='sclr')
-sclr.channels.read_attrs = ['chan1', 'chan2', 'chan3', 'chan4', 'chan5', 'chan6', 'chan7', 'chan8', 'chan9', 'chan10', 'chan11', 'chan12', 'chan13', 'chan14', 'chan15', 'chan16', 'chan17', 'chan18', 'chan19', 'chan20']
-sclr.channels.chan1.name = 'Clock'
-sclr.channels.chan2.name = 'I0'
-sclr.channels.chan3.name = 'PIN'
-sclr.channels.chan4.name = 'VxCl'
-sclr.channels.chan5.name = 'fbratio'
-sclr.channels.chan6.name = 'It'
-sclr.channels.chan7.name = 'If_tot'
-sclr.channels.chan8.name = 'S'
-sclr.channels.chan9.name = 'Mg'
-sclr.channels.chan10.name = 'P'
-sclr.channels.chan11.name = 'VxP'
-sclr.channels.chan12.name = 'Sr_Si'
-sclr.channels.chan13.name = 'Ca'
-sclr.channels.chan14.name = 'U'
-sclr.channels.chan15.name = 'VxAl'
-sclr.channels.chan16.name = 'VxMg'
-sclr.channels.chan17.name = 'VxS'
-sclr.channels.chan18.name = 'VxSi'
-sclr.channels.chan19.name = 'VxPu'
-sclr.channels.chan20.name = 'Cl'
+        return StatusBase()
+    
 
-sclr.count_mode.set('OneShot')  # make sure we are in the mode we expect
+class Scaler(Device):
+    # MCAs
+    mcas = C(ScalerMCA, '')
+    # TODO maybe an issue with the timing around the triggering?
+    cnts = C(ScalerCH, 'scaler1')
+    
+    def __init__(self, *args, mode='counting', **kwargs):
+        super().__init__(*args, **kwargs)
+        self.set_mode(mode)
+
+
+    def match_names(self, N=20):
+        self.cnts.match_names()
+        for j in range(1, N+1):
+            mca_ch = getattr(self.mcas.channels, f"mca{j:02d}")
+            ct_ch = getattr(self.cnts.channels, f"chan{j:02d}")
+            mca_ch.name = ct_ch.chname.get()
+
+    # TODO put a soft signal around this so we can stage it
+    def set_mode(self, mode):
+        if mode == 'counting':
+            self.read_attrs = ['cnts']
+            self.configuration_attrs = ['cnts']
+        elif mode == 'flying':
+            self.read_attrs = ['mcas']
+            self.configuration_attrs = ['mcas']
+        else:
+            raise ValueError
+
+        self._mode = mode
+
+    def trigger(self):
+        if self._mode == 'counting':
+            return self.cnts.trigger()
+        elif mode == 'flying':
+            return self.mcas.trigger()
+        else:
+            raise ValueError
+        
+    def stage(self):
+        self.match_names()
+        if self._mode == 'counting':
+            return self.cnts.stage()
+        elif mode == 'flying':
+            return self.mcas.stage()
+        else:
+            raise ValueError
+
+    def unstage(self):
+        if self._mode == 'counting':
+            return self.cnts.unstage()
+        elif mode == 'flying':
+            return self.mcas.unstage()
+        else:
+            raise ValueError
+
+
+sclr = Scaler('XF:08BM-ES:1{Sclr:1}', name='sclr')
+sclr.cnts.channels.read_attrs = [f"chan{j:02d}" for j in range(1, 21)]
+sclr.mcas.channels.read_attrs = [f"mca{j:02d}" for j in range(1, 21)]
+sclr.match_names(20)
+sclr.set_mode('counting')
+
+
+def tes_fly_struck(xstart, xstop, ystart, ystop, ysteps):
+    sclr.set_mode('flying')
+
+    for y in np.linspace(ystart, ystop, ysteps):
+        yield from bps.mv(x_mtr, x_start, y_mtr, y)
+        yield from bps.trigger(sclr, group='fly_row'))
+        yield from bps.abs_set(x_mtr, xstop, group='fly_row')
+
+        yield from bps.trigger_and_read([sclr])
+        
