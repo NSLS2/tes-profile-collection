c0b86877d5d4fa2a626472136fb9c3d41ae853c4
TES Operator <xf08bm@xf08bm-ws1.cs.nsls2.local>
2018-01-09 15:47:18 -0500

    ENH add untracked file

---
+++ startup/89-usercallbacks.py
@@ -0,0 +1,27 @@
+import tifffile
+
+class RasterMaker(CallbackBase):
+    def __init__(self, fname_template, field):
+        self.fname_template = fname_template
+        self.field = field
+        self.data = None
+        self.desc = None
+        self.fname = None
+          
+    def start(self, doc):
+        if 'shape' in doc:
+            self.data = np.ones(doc['shape']) * np.nan
+            self.fname = self.fname_template.format(start=doc)
+    def descriptor(self, doc):
+        if self.field in doc['data_keys']:
+             self.decs = doc['uid']
+    def event(self, doc):
+        if self.desc != doc['descriptor']:
+             return
+        self.data[np.unravel_index(doc['seq_num'], self.shape)] = doc['data'][self.field]
+    def stop(self, doc):
+        tifffile.imsave(self.fname, self.data)
+        self.data = None
+        self.desc = None
+        self.fname = None
+
