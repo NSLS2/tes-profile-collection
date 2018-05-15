f83c36f341074ec208220d1b603b0fff2a45f821
Thomas A Caswell <tcaswell@bnl.gov>
2018-05-15 19:09:38 -0400

    MNT: add delta-tau configuration PV
    Presumably there is more of the delta-tau we want to talk to.

--- startup/20-motors.py
+++ startup/20-motors.py
@@ -1,11 +1,16 @@
 from ophyd import (EpicsSignal, EpicsSignalRO, EpicsMotor,
-                   Device, Component as Cpt)
+                   Device, Component as Cpt, Signal)
+
 
 class XYStage(Device):
     x = Cpt(EpicsMotor, 'X}Mtr')
     y = Cpt(EpicsMotor, 'Y}Mtr')
 
+
 xy_stage = XYStage('XF:08BMES-OP{SM:1-Ax:', name='xy_stage')
 
 mono_energy = EpicsMotor('x08bm:mon', name='mono_energy')
 mono_energy.settle_time = 1.0
+
+dtt = EpicsSignal('XF:08BM-CT{MC:06}Asyn.AOUT', name='dtt',
+                  as_string=True)
