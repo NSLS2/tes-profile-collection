6e7f34de1300b26f0f739b4d43d62a77d22b0666
Thomas A Caswell <tcaswell@bnl.gov>
2019-02-19 17:34:08 -0500

    MNT: imports and whitespace

--- startup/91-userscans.py
+++ startup/91-userscans.py
@@ -1,37 +1,41 @@
-from bluesky.callbacks.broker import LiveTiffExporter, post_run
+from bluesky.callbacks.broker import (post_run, LiveGrid)
 
-xrfmapTiffOutputDir = '/home/xf08bm/DATA2017/Comissioning/20170619/' 
-#hard-coded for testing now; need to be set to automatically use SAF, today's date, etc.
+xrfmapTiffOutputDir = '/home/xf08bm/DATA2017/Comissioning/20170619/'
+# hard-coded for testing now; need to be set to automatically use SAF, today's date, etc.
 
 
-
-def xrfmap(*, xstart, xnumstep, xstepsize, 
-            ystart, ynumstep, ystepsize, 
-            rois = []
-	    #shutter = True, align = False,
-            #acqtime, numrois=1, i0map_show=True, itmap_show=False, record_cryo = False,
-            #setenergy=None, u_detune=None, echange_waittime=10
-            ):
-
+def xrfmap(*, xstart, xnumstep, xstepsize,
+           ystart, ynumstep, ystepsize,
+           rois=(),
+           # shutter=True,
+           # align=False,
+           # acqtime,
+           # numrois=1,
+           # i0map_show=True,
+           # itmap_show=False,
+           # record_cryo=False,
+           # setenergy=None,
+           # u_detune=None,
+           # echange_waittime=10
+           ):
     '''
     input:
          xstart, xnumstep, xstepsize (float)
          ystart, ynumstep, ystepsize (float)
-        
-    '''
 
-    #define detector used for xrf mapping functions
-    xrfdet = [sclr] #currently only the scalar; to-do: save full spectra
+    '''
+    # define detector used for xrf mapping functions
+    xrfdet = [sclr]  # currently only the scalar; to-do: save full spectra
 
     xstop = xstart + xnumstep*xstepsize
     ystop = ystart + ynumstep*ystepsize
 
-    #setup live callbacks:
+    # setup live callbacks:
 
     livetableitem = [xy_stage.x, xy_stage.y]
     livecallbacks = []
     callbackTokenList = []
-    
+
     for roi in rois:
         livecallbacks.append(LiveGrid((ynumstep+1, xnumstep+1), roi,
                                       xlabel='x (mm)',
@@ -39,28 +43,39 @@ def xrfmap(*, xstart, xnumstep, xstepsize,
                                       extent=[xstart, xstop, ystart, ystop]))
         livetableitem.append(roi)
 
-
-        #setup LiveOutput
-        xrfmapOutputTiffTemplate = xrfmapTiffOutputDir+"xrfmap_scan{start[scan_id]}" +roi+ ".tiff"
-#        xrfmapTiffexporter = LiveTiffExporter(roi, xrfmapOutputTiffTemplate, db=db)
-        xrfmapTiffexporter = RasterMaker(xrfmapOutputTiffTemplate, roi)        
+        # setup LiveOutput
+        xrfmapOutputTiffTemplate = (xrfmapTiffOutputDir +
+                                    "xrfmap_scan{start[scan_id]}" +
+                                    roi + ".tiff")
+        # xrfmapTiffexporter = LiveTiffExporter(roi, xrfmapOutputTiffTemplate, db=db)
+        xrfmapTiffexporter = RasterMaker(xrfmapOutputTiffTemplate, roi)
         callbackToken = RE.subscribe('all', post_run(xrfmapTiffexporter, db=db))
-        #check to see if this is the right way to have RE here...
+        # check to see if this is the right way to have RE here...
         callbackTokenList.append(callbackToken)
 
     livecallbacks.append(LiveTable(livetableitem))
-    
-    #setup LiveOutput
+
+    # setup LiveOutput
 
     if sclr in xrfdet:
         for sclrDataKey in [getattr(sclr.cnts.channels, f'chan{j:02d}') for d in range(1, 21)]:
-            xrfmapOutputTiffTemplate = xrfmapTiffOutputDir+"xrfmap_scan{start[scan_id]}" + sclrDataKey + ".tiff"
-#           xrfmapTiffexporter = LiveTiffExporter(roi, xrfmapOutputTiffTemplate, db=db)  #LiveTiffExporter exports one array from one event, commented out for future reference
-            xrfmapTiffexporter = RasterMaker(xrfmapOutputTiffTemplate, sclrDataKey)
-            callbackToken = RE.subscribe('all', post_run(xrfmapTiffexporter, db=db))#check to see if this is the right way to have RE here...
+            xrfmapOutputTiffTemplate = (xrfmapTiffOutputDir +
+                                        "xrfmap_scan{start[scan_id]}" +
+                                        sclrDataKey + ".tiff")
+
+            # xrfmapTiffexporter = LiveTiffExporter(roi, xrfmapOutputTiffTemplate, db=db)
+
+            # LiveTiffExporter exports one array from one event,
+            # commented out for future reference
+            xrfmapTiffexporter = RasterMaker(xrfmapOutputTiffTemplate,
+                                             sclrDataKey)
+            callbackToken = RE.subscribe('all', post_run(xrfmapTiffexporter, db=db))
+            # check to see if this is the right way to have RE here...
             callbackTokenList.append(callbackToken)
 
-    xrfmap_scanplan = outer_product_scan(xrfdet, xy_stage.y, ystart, ystop, ynumstep+1, xy_stage.x, xstart, xstop, xnumstep+1, False)
+    xrfmap_scanplan = outer_product_scan(xrfdet,
+                                         xy_stage.y, ystart, ystop, ynumstep + 1,
+                                         xy_stage.x, xstart, xstop, xnumstep + 1, False)
     xrfmap_scanplan = bp.subs_wrapper(xrfmap_scanplan, livecallbacks)
 
     scaninfo = yield from xrfmap_scanplan
@@ -69,4 +84,3 @@ def xrfmap(*, xstart, xnumstep, xstepsize,
         RE.unsubscribe(callbackToken)
 
     return xrfmap_scanplan
-
