0df867a327d3f1dd70aca7e460b9772203336359
Joshua Lynch <jlynch@bnl.gov>
2019-09-26 17:11:02 -0400

    Improve metadata captured in start document

--- startup/29-subscribe-suitcase.py
+++ startup/29-subscribe-suitcase.py
@@ -9,20 +9,19 @@ element_to_roi = {
 }
 
 
-#suitcase_meta_data = {"config": """\
 suitcase_config = """\
 [versions]
 "XDI"                         = "# XDI/1.0 Bluesky"
 
 [columns]
-"Column.1"                    = {column_label="energy",  data_key="E_centers", column_data="{configuration[E_centers][data][E_centers][0]}", units="eV"}
+"Column.1"                    = {column_label="energy",  data_key="E_centers", column_data="{[data][E_centers][0]}", units="eV"}
 "Column.2"                    = {column_label="I0",      data_key="I0", column_data="{data[I0][0]}"}
 "Column.3"                    = {column_label="If",      data_key="fluor", column_data="{data[fluor][0]}", transform="roi1"}
 
 [required_headers]
-"Element.symbol"              = {data="{md[XDI][Element_symbol]}"}
-"Element.edge"                = {data="{md[XDI][Element_edge]}"}
-"Mono.d_spacing"              = {data="{md[XDI][Mono_d_spacing]}"}
+"Element.symbol"              = {data="{md[XDI][Element_symbol]}", doc_name="start"}
+"Element.edge"                = {data="{md[XDI][Element_edge]}", doc_name="start"}
+"Mono.d_spacing"              = {data="{md[XDI][Mono_d_spacing]}", doc_name="start"}
 
 [optional_headers]
 
@@ -38,7 +37,7 @@ def roi1(event_doc, roi_lo_ndx, roi_hi_ndx):
 
 
 def serializer_factory(name, start_doc):
-    element = start_doc["element"]
+    element = start_doc["user_input"]["element"]
     roi = element_to_roi[element.lower()]
 
     suitcase_transforms = {
@@ -73,4 +72,5 @@ nx_meta_data = {
     "Beam": {"incident_energy": 1000.0},
 }
 
-RE.md.update({"md": {"suitcase-xdi": None, "NX": nx_meta_data, "XDI": xdi_meta_data}})
+# not like this
+#RE.md.update({"md": {"NX": nx_meta_data, "XDI": xdi_meta_data}})
