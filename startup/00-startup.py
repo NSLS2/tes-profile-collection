d3f66791a4a97787bf82fd88ba97a121700213c7
Thomas A Caswell <tcaswell@bnl.gov>
2019-09-26 17:19:03 -0400

    Apply black

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -1,9 +1,8 @@
 from nslsii import configure_base
 from IPython import get_ipython
 
-configure_base(get_ipython().user_ns, 'tes')
+configure_base(get_ipython().user_ns, "tes")
 
 
 # Optional: set any metadata that rarely changes.
-RE.md['beamline_id'] = 'TES'
-
+RE.md["beamline_id"] = "TES"
--- startup/91-userscans.py
+++ startup/91-userscans.py
@@ -6,31 +6,37 @@ from bluesky.plans import outer_product_scan
 # hard-coded for testing now; need to be set to automatically use SAF, today's date, etc.
 
 
-def xrfmap(*, xstart, xnumstep, xstepsize,
-           ystart, ynumstep, ystepsize,
-           rois=(),
-           # shutter=True,
-           # align=False,
-           # acqtime,
-           # numrois=1,
-           # i0map_show=True,
-           # itmap_show=False,
-           # record_cryo=False,
-           # setenergy=None,
-           # u_detune=None,
-           # echange_waittime=10
-           ):
-    '''
+def xrfmap(
+    *,
+    xstart,
+    xnumstep,
+    xstepsize,
+    ystart,
+    ynumstep,
+    ystepsize,
+    rois=(),
+    # shutter=True,
+    # align=False,
+    # acqtime,
+    # numrois=1,
+    # i0map_show=True,
+    # itmap_show=False,
+    # record_cryo=False,
+    # setenergy=None,
+    # u_detune=None,
+    # echange_waittime=10
+):
+    """
     input:
          xstart, xnumstep, xstepsize (float)
          ystart, ynumstep, ystepsize (float)
 
-    '''
+    """
     # define detector used for xrf mapping functions
     xrfdet = [sclr]  # currently only the scalar; to-do: save full spectra
 
-    xstop = xstart + xnumstep*xstepsize
-    ystop = ystart + ynumstep*ystepsize
+    xstop = xstart + xnumstep * xstepsize
+    ystop = ystart + ynumstep * ystepsize
 
     # setup live callbacks:
 
@@ -38,10 +44,15 @@ def xrfmap(*, xstart, xnumstep, xstepsize,
     livecallbacks = []
 
     for roi in rois:
-        livecallbacks.append(LiveGrid((ynumstep+1, xnumstep+1), roi,
-                                      xlabel='x (mm)',
-                                      ylabel='y (mm)',
-                                      extent=[xstart, xstop, ystart, ystop]))
+        livecallbacks.append(
+            LiveGrid(
+                (ynumstep + 1, xnumstep + 1),
+                roi,
+                xlabel="x (mm)",
+                ylabel="y (mm)",
+                extent=[xstart, xstop, ystart, ystop],
+            )
+        )
         livetableitem.append(roi)
 
     #     # setup LiveOutput
@@ -70,9 +81,18 @@ def xrfmap(*, xstart, xnumstep, xstepsize,
     #                                          sclrDataKey)
     #         livecallbacks.append(xrfmapTiffexporter)
 
-    xrfmap_scanplan = outer_product_scan(xrfdet,
-                                         xy_stage.y, ystart, ystop, ynumstep + 1,
-                                         xy_stage.x, xstart, xstop, xnumstep + 1, False)
+    xrfmap_scanplan = outer_product_scan(
+        xrfdet,
+        xy_stage.y,
+        ystart,
+        ystop,
+        ynumstep + 1,
+        xy_stage.x,
+        xstart,
+        xstop,
+        xnumstep + 1,
+        False,
+    )
     xrfmap_scanplan = bp.subs_wrapper(xrfmap_scanplan, livecallbacks)
 
     scaninfo = yield from xrfmap_scanplan
