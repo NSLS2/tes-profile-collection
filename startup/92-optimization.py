85f05b1f8e71f2d986b0130ebd7ee33b6944b7db
AbbyGiles <agiles@bnl.gov>
2020-08-27 16:10:28 -0400

    Rename imports

--- startup/92-optimization.py
+++ startup/92-optimization.py
@@ -1,5 +1,5 @@
-from bloptools.DE_opt_utils import run_hardware_fly
-from bloptools.DE_optimization import optimization_plan
+from bloptools.de_opt_utils import run_hardware_fly
+from bloptools.de_optimization import optimization_plan
 
 motor_dict = {sample_stage.x.name: {'position': sample_stage.x},
               sample_stage.y.name: {'position': sample_stage.y},
