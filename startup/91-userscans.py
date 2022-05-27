4fdc2f4f8f3abbba9a9748578c67c93d3b9e97c2
Maksim Rakitin <mrakitin@bnl.gov>
2022-05-27 12:05:40 -0400

    Better plan for scanning with random walk of 4 (or more) motors

--- startup/91-userscans.py
+++ startup/91-userscans.py
@@ -143,11 +143,70 @@ def get_random_walk(n, n_cycles, range=[-1, 1]):
 #     plt.plot(motor_positions[-1])
 
 
-def kb_trajectories(n, n_cycles, ranges=[]):
-    trajectories = []
-    for rng in ranges:
-        trajectories.append(get_random_walk(n, n_cycles, range=rng))
+def kb_trajectories(n, n_cycles, motors_ranges={}):
+    trajectories = {}
+    for mirror, motor_dict in motors_ranges.items():
+        for motor_name, rng in motor_dict.items():
+            trajectories[getattr(mirror, motor_name).name] = get_random_walk(n, n_cycles, range=rng)
     return trajectories
 
 # Example run:
 # kb_traj_list = kb_trajectories(1000, 16, ranges=[kbh_ranges["dsh"], kbh_ranges["ush"], kbv_ranges["dsh"], kbv_ranges["ush"]])
+
+
+def scan_with_random_walk(detectors=[vstream, I0, ring_current],
+                          motors_ranges={kbh: {"dsh": [-0.1/2, 0.15/2], "ush": [-0.2/2, 0.15/2]},
+                                         kbv: {"dsh": [-0.2/2, 0.15/2], "ush": [-0.2/2, 0.25/2]}},
+                          num_points=50000,
+                          num_cycles=800,
+                          testing=False,
+                          ):
+    """
+    kbh_ranges = {"dsh": [-0.1/2, 0.15/2], "ush": [-0.2/2, 0.15/2]}
+    kbv_ranges = {"dsh": [-0.2/2, 0.15/2], "ush": [-0.2/2, 0.25/2]}
+
+    kb_traj_list = kb_trajectories(50000, 800, ranges=[kbh_ranges["dsh"], kbh_ranges["ush"], kbv_ranges["dsh"], kbv_ranges["ush"]])
+
+    md = {"plan_args": {"detectors": [vstream.name, I0.name, ring_current.name],
+                        "args": [kbh.dsh.name, kbh.ush.name, kbv.dsh.name, kbv.ush.name,
+                                 'kb_trajectories(50000, 800, ranges=[kbh_ranges["dsh"], kbh_ranges["ush"], kbv_ranges["dsh"], kbv_ranges["ush"]])']},
+          "plan_pattern_args": "See 'plan_args'"}
+
+    RE(bp.rel_list_scan([vstream, I0, ring_current], kbh.dsh, kb_traj_list[0], kbh.ush, kb_traj_list[1], kbv.dsh, kb_traj_list[2], kbv.ush, kb_traj_list[3], md=md))
+    """
+    traj_dict = kb_trajectories(num_points, num_cycles, motors_ranges=motors_ranges)
+
+    detectors_str = [detector.name for detector in detectors]
+
+    motors = [getattr(mirror, motor_name) for mirror, motor_dict in motors_ranges.items()
+                                          for motor_name in motor_dict]
+    motors_str = [motor.name for motor in motors]
+
+    args = []
+    for motor in motors:
+        if testing:
+            args += [motor.name, traj_dict[motor.name]]  # testing
+        else:
+            args += [motor, traj_dict[motor.name]]  # production
+
+    motors_ranges_str = {mirror.name: motor_dict for mirror, motor_dict in motors_ranges.items()}
+
+    md = {"plan_args": {"detectors": [det.name for det in detectors],
+                        "args": [motors_str,
+                                 f'kb_trajectories({num_points}, {num_cycles}, motors_ranges={motors_ranges_str}']},
+          "plan_pattern_args": "See 'plan_args'"}
+
+    if testing:
+        print(f"bp.rel_list_scan({detectors_str}, *{args}, md={md})")  # testing
+    else:
+        yield from bp.rel_list_scan(detectors, *args, md=md)  # production
+
+
+# Example:
+"""
+RE(scan_with_random_walk(detectors=[vstream, I0, ring_current],
+                         motors_ranges={kbh: {"dsh": [-0.1/2, 0.15/2], "ush": [-0.2/2, 0.15/2]},
+                                        kbv: {"dsh": [-0.2/2, 0.15/2], "ush": [-0.2/2, 0.25/2]}},
+                         num_points=50000,
+                         num_cycles=800))
+"""
