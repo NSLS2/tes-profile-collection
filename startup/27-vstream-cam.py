bfed5c8900b97b837ae01419ce43b7d2f736ee8a
Maksim Rakitin <mrakitin@bnl.gov>
2022-05-27 16:09:50 -0400

    `vstream` with a single HDF5 file per scan

--- startup/27-vstream-cam.py
+++ startup/27-vstream-cam.py
@@ -2,6 +2,7 @@ import datetime
 import logging
 import time as ttime
 from collections import deque
+import itertools
 from pathlib import Path
 
 import cv2
@@ -55,9 +56,10 @@ class VideoStreamDet(Device):
         self._resource_document = None
         self._datum_factory = None
 
-    def trigger(self, *args, **kwargs):
-        super().trigger(*args, **kwargs)
+        self._frame_shape = (480, 704)
 
+    def stage(self):
+        super().stage()
         date = datetime.datetime.now()
         self._assets_dir = date.strftime("%Y/%m/%d")
         data_file = f"{new_uid()}.h5"
@@ -69,14 +71,29 @@ class VideoStreamDet(Device):
             resource_path=str(Path(self._assets_dir) / Path(data_file)),
             resource_kwargs={},
         )
+
+        self._data_file = str(
+            Path(self._resource_document["root"])
+            / Path(self._resource_document["resource_path"])
+        )
+
         # now discard the start uid, a real one will be added later
         self._resource_document.pop("run_start")
         self._asset_docs_cache.append(("resource", self._resource_document))
 
-        data_file = str(
-            Path(self._resource_document["root"])
-            / Path(self._resource_document["resource_path"])
-        )
+        print(f"{self._data_file = }")
+
+        self._h5file_desc = h5py.File(self._data_file, "w")
+        group = self._h5file_desc.create_group("/entry")
+        self._dataset = group.create_dataset("averaged",
+                                             data=np.zeros((1, *self._frame_shape)),
+                                             maxshape=(None, *self._frame_shape),
+                                             dtype="float64",
+                                             compression="lzf")
+        self._counter = itertools.count()
+
+    def trigger(self, *args, **kwargs):
+        super().trigger(*args, **kwargs)
 
         frames = []
         times = []
@@ -105,30 +122,29 @@ class VideoStreamDet(Device):
         # Averaging over all frames and summing 3 RGB channels
         averaged = frames.mean(axis=0).sum(axis=-1)
 
-        with h5py.File(data_file, "w") as f:
-            group = f.create_group("/entry")
-            group.create_dataset("frames", data=frames, compression="lzf")
-            group.create_dataset("averaged", data=averaged, compression="lzf")
+        current_frame = next(self._counter)
+        self._dataset.resize((current_frame + 1, *self._frame_shape))
+        print(f"{self._dataset = }\n{self._dataset.shape = }")
+        self._dataset[current_frame, :, :] = averaged
 
-        datum_document = self._datum_factory(datum_kwargs={})
+        datum_document = self._datum_factory(datum_kwargs={"frame": current_frame})
         self._asset_docs_cache.append(("datum", datum_document))
 
         self.image.put(datum_document["datum_id"])
         self.mean.put(averaged.mean())
 
-        self._resource_document = None
-        self._datum_factory = None
-
         return NullStatus()
 
     def describe(self):
         res = super().describe()
-        res[self.image.name].update(dict(shape=(480, 704)))
+        res[self.image.name].update(dict(shape=self._frame_shape))
         return res
 
     def unstage(self):
         super().unstage()
+        self._h5file_desc.close()
         self._resource_document = None
+        self._datum_factory = None
 
     def collect_asset_docs(self):
         items = list(self._asset_docs_cache)
@@ -144,20 +160,16 @@ vstream = VideoStreamDet(
 )
 vstream.exposure_time.put(0.25)
 
-
-class VideoStreamHDF5Handler(HandlerBase):
-    specs = {"VIDEO_STREAM_HDF5"}
-
-    def __init__(self, filename):
-        self._name = filename
-
-    def __call__(self):
-        with h5py.File(self._name, "r") as f:
-            entry = f["/entry/averaged"]
-            return entry[:]
-
-
-db.reg.register_handler("VIDEO_STREAM_HDF5", VideoStreamHDF5Handler, overwrite=True)
+# This is not needed with tiled/databroker 2.0+ (done via the `tiled-site-config` repo).
+# class VideoStreamHDF5Handler(HandlerBase):
+#     specs = {"VIDEO_STREAM_HDF5"}
+#     def __init__(self, filename):
+#         self._name = filename
+#     def __call__(self):
+#         with h5py.File(self._name, "r") as f:
+#             entry = f["/entry/averaged"]
+#             return entry[:]
+# db.reg.register_handler("VIDEO_STREAM_HDF5", VideoStreamHDF5Handler, overwrite=True)
 
 # Logger config:
 # handler = logging.StreamHandler()
