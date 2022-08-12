58eee685d6cf83eda8ed2aad9accb20e614fab06
Maksim Rakitin <mrakitin@bnl.gov>
2022-08-12 15:07:06 -0400

    Scan with delay for feedback to fix I0, etc.

--- startup/91-userscans.py
+++ startup/91-userscans.py
@@ -211,3 +211,13 @@ RE(scan_with_random_walk(detectors=[vstream, I0, ring_current],
                          num_points=50000,
                          num_cycles=800))
 """
+
+
+@bpp.stage_decorator([vstream] + [mono.energy])
+@bpp.run_decorator()
+def scan_with_delay(dets, motor, start, stop, num_points, sleep=3.0):
+    positions = np.linspace(start, stop, num_points)
+    for pos in positions:
+        yield from bps.mv(motor, pos)
+        yield from bps.sleep(sleep)
+        yield from bpp.trigger_and_read(dets + [motor])
