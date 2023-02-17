e8af7e97729191d1faec5e12fded9c85329c7182
Maksim Rakitin <mrakitin@bnl.gov>
2023-02-17 17:00:17 -0500

    CI: use the PR no.5 branch of bloptools

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -4,3 +4,12 @@
 
 sudo mkdir -p -v /nsls2/xf08bm/shared/config/runengine-metadata
 sudo chown -Rv $USER: /nsls2/
+
+# Temporary solution until the bloptools#5 is merged/released.
+#
+#   https://github.com/NSLS-II/bloptools/pull/5
+#
+mkdir ~/src/ && src/
+git clone https://github.com/thomaswmorris/bloptools.git
+cd bloptools
+git checkout gp-opt
