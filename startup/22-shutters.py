aab59a2f25dffee4b17aaf010ca31743ddfc88d4
Maksim Rakitin <mrakitin@bnl.gov>
2023-02-19 14:54:41 -0500

    Address PR review suggestions/add comments about the shutter class

--- startup/22-shutters.py
+++ startup/22-shutters.py
@@ -1,14 +1,22 @@
 from nslsii.devices import TwoButtonShutter
 
-#psh = TwoButtonShutter("XF:08BMES-PPS{PSh}", name="psh")
+
+# TODO: This shutter class needs some work to account for the cases of multiple
+# and prolonged state switches.
+#
+#   https://github.com/NSLS-II-TES/profile_collection/issues/34
+#
+# psh = TwoButtonShutter("XF:08BMES-PPS{PSh}", name="psh")
+# Usage:
+#   psh.set("Open").wait()
+#   psh.set("Close").wait()
+
 
 class PShutter(Device):
+    # TODO: extend the class to take care of the flaky state switching.
     open_cmd = Cpt(EpicsSignal, "Cmd:Opn-Cmd")
     close_cmd = Cpt(EpicsSignal, "Cmd:Cls-Cmd")
     status = Cpt(EpicsSignalRO, "Pos-Sts")
-    
+
+
 psh = PShutter("XF:08BMES-PPS{PSh}", name="psh")
-
-
-# psh.set("Open").wait()
-# psh.set("Close").wait()
